104675b53dfdadc6afdf25dec89e6a096d95b2cf
ALex Nikonov <alex@nikonov.tech>
2024-10-17 00:52:06 +0400

    refactor: move thing here and there

--- lua/plugins/lsp_signature.lua
+++ lua/plugins/lsp_signature.lua
@@ -1,3 +1,87 @@
+-- show references with Telescope, much better to see the context
+vim.lsp.handlers["textDocument/references"] = function(_, _, _) require("telescope.builtin").lsp_references() end
+vim.lsp.handlers["textDocument/implementation"] = function(_, _, _) require("telescope.builtin").lsp_implementations() end
+
+-- tune up parameters to Go LSP
+require("lspconfig").gopls.setup {
+  single_file_support = true,
+  settings = {
+    gopls = {
+      symbolScope = "all",
+      symbolStyle = "Dynamic",
+      symbolMatcher = "FastFuzzy",
+      importShortcut = "Both",
+      hoverKind = "FullDocumentation", -- or "SynopsisDocumentation", "SingleLine"
+      completeUnimported = true,
+      usePlaceholders = false,
+      hints = {
+        assignVariableTypes = true,
+        compositeLiteralFields = true,
+        compositeLiteralTypes = true,
+        constantValues = true,
+        functionTypeParameters = true,
+        parameterNames = true,
+        rangeVariableTypes = true,
+      },
+      analyses = {
+        unusedparams = true,
+        unusedvariable = true,
+        assign = true,
+        atomic = true,
+        bools = true,
+        composites = true,
+        copylocks = true,
+        defers = true,
+        deprecated = true,
+        directive = true,
+        embed = true,
+        errorsas = true,
+        fillreturns = true,
+        httpresponse = true,
+        ifaceassert = true,
+        infertypeargs = true,
+        loopclosure = true,
+        lostcancel = true,
+        nilfunc = true,
+        nilness = true,
+        nonewvars = true,
+        noresultvalues = true,
+        printf = true,
+        shadow = true, -- MAYBE NOT
+        shift = true,
+        sigchanyzer = true,
+        simplifycompositelit = true,
+        simplifyrange = true,
+        simplifyslice = true,
+        sortslice = true,
+        stdmethods = true,
+        stringintconv = true,
+        structtag = true,
+        testinggoroutine = true,
+        tests = true,
+        timeformat = true,
+        unmarshal = true,
+        unreachable = true,
+        unsafeptr = true,
+        unusedresult = true,
+        unusedwrite = true,
+        useany = true,
+      },
+      staticcheck = true,
+      gofumpt = true,
+      diagnosticsDelay = "1s",
+      diagnosticsTrigger = "Edit",
+      analysisProgressReporting = true,
+      annotations = {
+        bounds = true,
+        escape = true,
+        inline = true,
+        -- 'nil' = true,
+      },
+    },
+  },
+}
+
 return {
   "ray-x/lsp_signature.nvim",
 
@@ -13,5 +97,4 @@ return {
       border = "rounded", -- Optional: border for floating window
     },
   },
-  config = function(_, opts) require("lsp_signature").setup(opts) end,
 }
