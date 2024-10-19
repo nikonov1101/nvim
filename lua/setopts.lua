d8d45093a70c1fd3dcc30ea029e0d0e9b2b2d593
ALex Nikonov <alex+redot@nikonov.tech>
2024-10-20 00:43:06 +0400

    use built-in folding

--- lua/setopts.lua
+++ lua/setopts.lua
@@ -1,3 +1,4 @@
+vim.opt.cursorline = true
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv "HOME" .. "/.vim/undodir"
@@ -21,3 +22,13 @@ vim.opt.termguicolors = true
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
+
+-- trying to use built-in folding
+vim.opt.foldmethod = "marker"
+-- :h vim.treesitter.foldexpr()
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+-- ref: https://github.com/neovim/neovim/pull/20750
+vim.opt.foldtext = ""
+vim.opt.fillchars:append("fold: ")
+-- Open all folds by default, zm is not available
+vim.opt.foldlevelstart = 99
