e446ee9d0c71d5a7847a07ae3ba42ab8cebe99ed
ALex Nikonov <alex@nikonov.tech>
2024-10-21 13:00:10 +0400

    fix: set luasnip as top prio

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -173,8 +173,9 @@ return {
                 ["<C-Space>"] = cmp.mapping.complete(),
             }),
             sources = cmp.config.sources({
-                { name = 'nvim_lsp_signature_help' }, -- function signatures
+                -- keep luasnip first
                 { name = 'luasnip' },                 -- For luasnip users.
+                { name = 'nvim_lsp_signature_help' }, -- function signatures
                 { name = 'nvim_lsp' },
                 { name = 'buffer' },
             })
