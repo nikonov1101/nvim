4e94500e1c6bab9a4f4fd3dec477cd048a4d9d21
ALex Nikonov <alex@nikonov.tech>
2024-10-28 16:54:56 +0400

    plug: ufo: disable fold column

--- lua/plugins/ufo.lua
+++ lua/plugins/ufo.lua
@@ -5,16 +5,18 @@ return {
     },
     config = function()
         local ufo = require("ufo")
+
+        vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+        vim.o.foldlevelstart = 99
+        -- hide fold column
+        vim.o.foldcolumn = '0'
+        vim.o.foldenable = false
+
         ufo.setup({
             provider_selector = function(_, _, _)
                 return { 'treesitter', 'indent' }
             end
         })
-
-        vim.o.foldcolumn = '1' -- '0' is not bad
-        vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
-        vim.o.foldlevelstart = 99
-        vim.o.foldenable = true
 
         vim.keymap.set('n', 'zR', ufo.openAllFolds)
         vim.keymap.set('n', 'zM', ufo.closeAllFolds)
