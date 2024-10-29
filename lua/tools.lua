c1ff08508467bc07f33c12a37d01732f1d7a60b6
ALex Nikonov <alex@nikonov.tech>
2024-10-29 12:31:38 +0400

    aux: extract tools

---
+++ lua/tools.lua
@@ -0,0 +1,26 @@
+local M = {}
+
+function M.dump_table(t)
+    if type(t) == 'table' then
+        local s = '{ '
+        for k, v in pairs(t) do
+            if type(k) ~= 'number' then k = '"' .. k .. '"' end
+            s = s .. '[' .. k .. '] = ' .. M.dump_table(v) .. ', '
+        end
+        return s .. '} '
+    else
+        return tostring(t)
+    end
+end
+
+function M.len_of(t)
+    local count = 0
+    for _ in pairs(t) do count = count + 1 end
+    return count
+end
+
+function M.cwd_basename()
+    return vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+end
+
+return M
--- play.lua
+++ play.lua
@@ -1,23 +1,7 @@
--- print lua table
-function dump(o)
-    if type(o) == 'table' then
-        local s = '{ '
-        for k, v in pairs(o) do
-            if type(k) ~= 'number' then k = '"' .. k .. '"' end
-            s = s .. '[' .. k .. '] = ' .. dump(v) .. ', '
-        end
-        return s .. '} '
-    else
-        return tostring(o)
-    end
-end
+local tools = require("lua.tools")
+print(tools.dump_table(tools))
 
--- todo: do we need a "tools" package?
-local function tablelength(T)
-    local count = 0
-    for _ in pairs(T) do count = count + 1 end
-    return count
-end
-
--- print(vim.fn.fnamemodify(vim.fn.getcwd(), ":t"))
--- print(vim.fn.stdpath("data"))
+local t = { "qwe", "rty", "foo", "bar" }
+print(tools.dump_table(t))
+print(tools.len_of(t))
+print(tools.cwd_basename())
