e54ce428087d0f602f02d7fa57733be8fffc057e
ALex Nikonov <alex@nikonov.tech>
2024-11-04 01:38:23 +0400

    plug: lazydev added

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -11,6 +11,19 @@ return {
         "hrsh7th/cmp-nvim-lsp-signature-help",
         "L3MON4D3/LuaSnip",
         "saadparwaiz1/cmp_luasnip",
+        { 'Bilal2453/luvit-meta', lazy = true },
+        {
+            -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
+            -- used for completion, annotations and signatures of Neovim apis
+            'folke/lazydev.nvim',
+            ft = 'lua',
+            opts = {
+                library = {
+                    -- Load luvit types when the `vim.uv` word is found
+                    { path = 'luvit-meta/library', words = { 'vim%.uv' } },
+                },
+            },
+        },
     },
 
     config = function()
