77633d96a7ee10e58cbc1ba4674a783ab7f96e9e
ALex Nikonov <alex@nikonov.tech>
2024-11-04 23:05:23 +0400

    lsp: properly apply code actions

--- lua/autogroups.lua
+++ lua/autogroups.lua
@@ -23,17 +23,24 @@ autocmd("BufWritePre", {
     pattern = "*",
     command = [[%s/\s\+$//e]],
 })
-autocmd("BufWritePre", {
-    group = the_autoformat_group,
-    pattern = "*",
-    callback = function() vim.lsp.buf.format() end,
-})
 autocmd("bufwritepre", {
     group = the_autoformat_group,
     pattern = "*.go",
     callback = function()
-        vim.lsp.buf.code_action({ context = { only = { "source.organizeimports" } }, apply = true })
-        vim.lsp.buf.code_action({ context = { only = { "source.fixall" } }, apply = true })
+        local params = vim.lsp.util.make_range_params()
+        params.context = { only = { "source.organizeImports" } }
+        local timeout_ms = 1000
+        local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, timeout_ms)
+        for cid, res in pairs(result or {}) do
+            for _, r in pairs(res.result or {}) do
+                if r.edit then
+                    local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+                    vim.lsp.util.apply_workspace_edit(r.edit, enc)
+                end
+            end
+        end
+
+        vim.lsp.buf.format({ async = false })
     end,
 })
 autocmd("bufwritepre", {
