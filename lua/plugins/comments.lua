e620d11b06437a4e36330721de84d97a427ca49b
ALex Nikonov <alex@nikonov.tech>
2024-11-04 13:16:20 +0400

    comments: simplify config, enable block comments

--- lua/plugins/comments.lua
+++ lua/plugins/comments.lua
@@ -1,31 +1,17 @@
 return {
     "numToStr/Comment.nvim",
-    config = function()
-        local c = require("Comment")
-        c.setup()
-
-        vim.keymap.set(
-            "n",
-            "<Leader>/",
-            function()
-                return require("Comment.api").call(
-                    "toggle.linewise." .. (vim.v.count == 0 and "current" or "count_repeat"),
-                    "g@$"
-                )()
-            end,
-
-            {
-                expr = true,
-                silent = true,
-                desc = "Toggle comment line",
-            }
-        )
-
-        vim.keymap.set(
-            "x",
-            "<Leader>/",
-            "<Esc><Cmd>lua require('Comment.api').locked('toggle.linewise')(vim.fn.visualmode())<CR>",
-            { desc = "Toggle comment" }
-        )
-    end,
+    opts = {
+        toggler = {
+            line = "<Leader>/",
+            block = "<Leader>?",
+        },
+        opleader = {
+            line = "<Leader>/",
+            block = "<Leader>?",
+        },
+        mappings = {
+            basic = true,
+            extra = false,
+        },
+    },
 }
