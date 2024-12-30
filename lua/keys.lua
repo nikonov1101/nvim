614c9abf97e7a426048f27bd24c28adeddb4ebad
ALex Nikonov <alex@nikonov.tech>
2024-12-30 14:38:37 +0400

    keys: undo the undo

--- lua/keys.lua
+++ lua/keys.lua
@@ -15,6 +15,8 @@ vim.keymap.set({ "n", "v" }, "<down>", "")
 vim.keymap.set({ "n", "v" }, "H", "B")
 vim.keymap.set({ "n", "v" }, "L", "W")
 vim.keymap.set({ "n", "v" }, "M", "E")
+-- undo the undo
+vim.keymap.set("n", "U", "<cmd>redo<cr>")
 
 -- better movements in insert-mode
 vim.keymap.set("i", "jj", "<Esc>", with_opts())
